a8637d3091f728e8b466c40e85518cfa5d69b4d9
Juraj Mičko <micko.juraj64@gmail.com>
2017-04-12 14:00:59 +0200

    Implement QueryCache

--- src/main/java/com/jjurm/projects/mpp/map/DaylightMap.java
+++ src/main/java/com/jjurm/projects/mpp/map/DaylightMap.java
@@ -1,14 +1,9 @@
 package com.jjurm.projects.mpp.map;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 
-import com.jjurm.projects.mpp.db.DatabaseManager;
+import com.jjurm.projects.mpp.db.QueryCache;
 import com.jjurm.projects.mpp.model.Attendant;
 import com.jjurm.projects.mpp.model.Place;
 
@@ -16,6 +11,9 @@ public class DaylightMap extends ProductivityMap {
 
   public static final double D = 100.0 / 121.0;
   public static final double WORKING_HOURS = 16;
+
+  public static QueryCache<Integer, Double> cache = new QueryCache<Integer, Double>(
+      day -> "SELECT declination FROM sundeclination WHERE `day` = " + day, rs -> rs.getDouble(1));
 
   public DaylightMap(Date date, Attendant attendant) {
     super(date, attendant);
@@ -28,7 +26,7 @@ public class DaylightMap extends ProductivityMap {
     Calendar calendar = Calendar.getInstance();
     calendar.setTime(date);
     int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR) + day;
-    double declination = DeclinationCache.getDeclination(dayOfYear) * Math.PI / 180;
+    double declination = cache.getValue(dayOfYear) * Math.PI / 180;
 
     double h = Math.acos(-Math.tan(phi) * Math.tan(declination)) / (2 * Math.PI);
     double sunrise = 24 * -h + 12;
@@ -37,37 +35,7 @@ public class DaylightMap extends ProductivityMap {
     double RL = Math.max(0, -sunset + sunrise + WORKING_HOURS) / WORKING_HOURS;
 
     double P = RL * D + (1 - RL);
-
     return P;
-  }
-
-  static class DeclinationCache {
-
-    private static HashMap<Integer, Double> cache = new HashMap<Integer, Double>();
-
-    public static Double getDeclination(int dayOfYear) {
-
-      if (cache.containsKey(dayOfYear)) {
-        return cache.get(dayOfYear);
-      }
-
-      String query = "SELECT declination FROM sundeclination WHERE `day` = " + dayOfYear;
-      try (Connection conn = DatabaseManager.getConnection();
-          Statement stmt = conn.createStatement();
-          ResultSet result = stmt.executeQuery(query);) {
-        if (result.first()) {
-          Double declination = result.getDouble(1);
-          cache.put(dayOfYear, declination);
-          return declination;
-        } else {
-          return null;
-        }
-      } catch (SQLException e) {
-        e.printStackTrace();
-        return null;
-      }
-    }
-
   }
 
 }
